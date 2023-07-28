1d05816d8a5391c680ea90c49221d46c63873514
softcat477 <softcat477@gmail.com>
2023-07-28 11:43:11 -0400

    Add Player movement and fire bullets
    - WASD to move with rigidbody2d.MovePosition
    - Aim using cursor position
    - Left-click to fire bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    // Destroy gameobject after aliveTime;
+    public float aliveTime = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CountDown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator CountDown() {
+        yield return new WaitForSeconds(aliveTime);
+        Destroy(gameObject);
+    }
+}
