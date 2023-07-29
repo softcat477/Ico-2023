59520c5c4ed73ec9cc6cddd01bce0adcb10367bd
softcat477 <softcat477@gmail.com>
2023-07-29 19:37:29 -0400

    Add FleeEnemy animation

--- Assets/Scripts/FleeEnemy.cs
+++ Assets/Scripts/FleeEnemy.cs
@@ -12,6 +12,7 @@ public class FleeEnemy : MonoBehaviour
 
     public int enemyHealth = 1;
     int health;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,7 @@ public class FleeEnemy : MonoBehaviour
         target = GameObject.FindWithTag("Player").transform;
 
         health = enemyHealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,6 +31,7 @@ public class FleeEnemy : MonoBehaviour
     {
         Vector3 target2enemy = transform.position - target.position;
         agent.SetDestination(transform.position + target2enemy.normalized * fleeSpeed);
+        animator.SetFloat("X Direction", target2enemy.x);
     }
 
     void OnTriggerEnter2D(Collider2D collider) {
