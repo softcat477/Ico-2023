4a9eb27a2ad8acdc47bb9481f81ea33d47d9354f
softcat <softcat477@gmail.com>
2023-07-30 15:36:52 -0400

    Add chase enenmy animation

--- Assets/Scripts/ChaseEnemy.cs
+++ Assets/Scripts/ChaseEnemy.cs
@@ -13,6 +13,8 @@ public class ChaseEnemy : MonoBehaviour
     public GameObject deathAnimation;
     public float deathAnimationdelay = 1.0f;
 
+    Animator animator;
+
     public SpriteRenderer renderer;
     // Start is called before the first frame update
     void Start()
@@ -24,12 +26,17 @@ public class ChaseEnemy : MonoBehaviour
         target = GameObject.FindWithTag("Player").transform;
 
         health = enemyHealth;
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(target.transform.position);
+
+        Vector3 target2enemy = transform.position - target.position;
+        animator.SetFloat("X Direction", -target2enemy.x);
     }
 
     void OnTriggerEnter2D(Collider2D collider) {
